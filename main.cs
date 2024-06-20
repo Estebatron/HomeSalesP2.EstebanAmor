e452fac92a43e62a327880266b44b2cb01d47e30
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-20 01:37:16 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+      var initial1 = "";
+      var initial2 = "";
+      var initial3 = "";
+    
+    String[] names = {"Danielle", "Edward", "Frasncis"};
+    String[] allowedInitials = {"d", "D", "e", "E", "f", "F"};
+    
+      int sale1 = 0;
+      int sale2 = 0;
+      int sale3 = 0;
+        int total = sale1 + sale2 +sale3;
+    
+    int[] sales = {sale1, sale2, sale3};
+do{
+    Console.WriteLine("Enter the initial of the first salesperson");
+      initial1 = Console.ReadLine();
+    foreach (var item in allowedInitials)
+        {
+         if( )
+        }
+    else {
+           Console.WriteLine("Please enter a valid initial");
+          }
+  
+  }while(true);
+}
+  }
